e33a2ecb5e71436374015b9aa44832bb7dacd544
Brendten Eickstaedt <brendten@brendteneickstaedt.com>
2013-03-15 14:42:14 -0400

    commented girl_friday queues

--- config/initializers/girl_friday.rb
+++ config/initializers/girl_friday.rb
@@ -1,3 +1,4 @@
+# background "job" that scores all existing Articles against a query and routes the matching documents
 ROUTE_NEW_QUERY = GirlFriday::WorkQueue.new(:route_new_query) do |msg|
   query = Query.where(id: msg[:query_id]).first unless msg.blank? || msg[:query_id].blank?
   ArticleRouter.instance.route_on_new_query(query) unless query.blank?
