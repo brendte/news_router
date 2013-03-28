a10cce2f25d2c993b440afaba7c51f8ce861f93b
Brendten Eickstaedt <brendten@brendteneickstaedt.com>
2013-03-28 08:57:14 -0400

    turn off scheduled job

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,29 +1,29 @@
-# code to perform background, recurring CRAWL_INDEX_ROUTE job
-# this is what trigger the fetching, indexing and routing of new documents as they appear in the Feeds
-# this code is run by RufusScheduler every 30 minutes
-# uses rufus_scheduler gem (https://github.com/jmettraux/rufus-scheduler) as an in-process recurring job scheduler/runner;
-# we use this mainly because it is expensive to run cron jobs on Heroku since they run in a separate process (which costs money :)
-unless Rails.env.to_sym == :development
-  scheduler = Rufus::Scheduler.start_new
-
-  scheduler.every('30m', first_in: '5m') do
-    #synchronized on Index::index_mutex to ensure multiple jobs won't run concurrently
-    if Indexer.index_mutex.try_lock
-      Rails.logger.info "Crawler#crawl task started at #{Time.now.utc}"
-      Crawler.instance.crawl
-      Rails.logger.info "Crawler#crawl task finished at #{Time.now.utc}"
-      Rails.logger.info "Article::index_new task started at #{Time.now.utc}"
-      Article.index_new
-      Rails.logger.info "Article::index_new task finished at #{Time.now.utc}"
-      Rails.logger.info "Query::index_new task started at #{Time.now.utc}"
-      Query.index_new
-      Rails.logger.info "Query::index_new task finished at #{Time.now.utc}"
-      Rails.logger.info "ArticleRouter#route_new task started at #{Time.now.utc}"
-      ArticleRouter.instance.route_new
-      Rails.logger.info "ArticleRouter#route_new task finished at #{Time.now.utc}"
-      Indexer.index_mutex.unlock
-    else
-      Rails.logger.info "Couldn't obtain lock on Index::index_mutex. Will try again in an hour (around #{Time.now.utc + 3600}. Bye!"
-    end
-  end
-end
+## code to perform background, recurring CRAWL_INDEX_ROUTE job
+## this is what trigger the fetching, indexing and routing of new documents as they appear in the Feeds
+## this code is run by RufusScheduler every 30 minutes
+## uses rufus_scheduler gem (https://github.com/jmettraux/rufus-scheduler) as an in-process recurring job scheduler/runner;
+## we use this mainly because it is expensive to run cron jobs on Heroku since they run in a separate process (which costs money :)
+#unless Rails.env.to_sym == :development
+#  scheduler = Rufus::Scheduler.start_new
+#
+#  scheduler.every('30m', first_in: '5m') do
+#    #synchronized on Index::index_mutex to ensure multiple jobs won't run concurrently
+#    if Indexer.index_mutex.try_lock
+#      Rails.logger.info "Crawler#crawl task started at #{Time.now.utc}"
+#      Crawler.instance.crawl
+#      Rails.logger.info "Crawler#crawl task finished at #{Time.now.utc}"
+#      Rails.logger.info "Article::index_new task started at #{Time.now.utc}"
+#      Article.index_new
+#      Rails.logger.info "Article::index_new task finished at #{Time.now.utc}"
+#      Rails.logger.info "Query::index_new task started at #{Time.now.utc}"
+#      Query.index_new
+#      Rails.logger.info "Query::index_new task finished at #{Time.now.utc}"
+#      Rails.logger.info "ArticleRouter#route_new task started at #{Time.now.utc}"
+#      ArticleRouter.instance.route_new
+#      Rails.logger.info "ArticleRouter#route_new task finished at #{Time.now.utc}"
+#      Indexer.index_mutex.unlock
+#    else
+#      Rails.logger.info "Couldn't obtain lock on Index::index_mutex. Will try again in an hour (around #{Time.now.utc + 3600}. Bye!"
+#    end
+#  end
+#end
